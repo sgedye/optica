d4f6e2d4313f65e3b1f79c66f97fd13dee9ef578
Shaun Gedye <sgedye88@yahoo.com.au>
2020-01-27 21:39:52 +1100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@
 /* This file app.js was written by Shaun Gedye */
 /***********************************************/
 
+function toggleText(content) {
+  if (content.classList.contains('hide')) {
+    content.classList.remove('hide');
+    content.classList.add('show');
+  } else if (content.classList.contains('show')) {
+    content.classList.remove('show');
+    content.classList.add('hide');
+  }
+}
 function activeButton(btnPressed) {
   this.buttons = document.getElementById('btn-filters');
   for (let i=0; i<this.buttons.childElementCount; i++) {
@@ -30,8 +39,22 @@ function filterStore(selection) {
 }
 /* Event Listeners */
 function eventListeners() {
+  const articles = document.querySelectorAll('.article');
+  articles.forEach(article => {
+    article.addEventListener('click', (event) => {
+      let articleToToggle = null;
+      if (event.target.classList.contains('article')) {
+        articleToToggle = event.target;
+      } else if (event.target.parentElement.classList.contains('article')) {
+        articleToToggle = event.target.parentElement;
+      } else if (event.target.parentElement.parentElement.classList.contains('article')) {
+        articleToToggle = event.target.parentElement.parentElement;
+      }
+      toggleText(articleToToggle.children[2]);
+    });
+  });
   const filterBtn = document.querySelectorAll('.filter-btn');
-  filterBtn.forEach((btn) => {
+  filterBtn.forEach(btn => {
     btn.addEventListener('click', (event) => {
       activeButton(event.target);
       filterStore(event.target.dataset.filter);
